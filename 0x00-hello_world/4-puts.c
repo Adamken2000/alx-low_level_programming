1f86c54cff357add6eee995da8aab06ef1498356
Kenneth Mzungu <adamken91@gmail.com>
2023-06-15 18:22:12 -0700

    my first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A program printing line with puts function
+ * Return: 0 (Complete)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
